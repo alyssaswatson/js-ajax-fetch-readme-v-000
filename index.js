2fb6e02216affe4b97b71afe35aa9311c33f9ceb
Alyssa Watson <alyssa.s.watson@gmail.com>
2018-08-18 22:26:42 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-const app = "I don't do much.";
+const token = 'bf756d8c42edbaa0d27e4c0c3a13dfb58ca1b1fe'
+fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+}).then(res => res.json()).then(json => console.log(json));
